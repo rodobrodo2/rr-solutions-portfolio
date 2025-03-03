efb14f117ea7a79e84b1089745a36b82afaccfcd
Rodobrodo2 <rodondolo2@gmail.com>
2025-03-03 14:08:57 +0100

    d

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -20,7 +20,7 @@ export const Home = () => {
                         <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_btnMenu]">
                             View Projects
                         </a>
-                        <a href="#projects" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_btnMenu] hover:bg-blue-500/10">
+                        <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_btnMenu] hover:bg-blue-500/10">
                             Contact Me
                         </a>
                     </div>
